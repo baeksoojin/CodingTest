16239ee54ce367c1e9f5d910a0cd4af0b5d7e31e
sujin-create <qsoojinq@naver.com>
2022-12-31 20:42:16 +0900

    floydwarshall

---
+++ 최단경로/플로이드워셜/플로이드워셜.py
@@ -0,0 +1,30 @@
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a==b:
+            graph[a][b] = 0
+
+
+for _ in range(m):
+    # a에서 b로 가는 비용을 입력받음
+    a,b,cost = map(int, input().split())
+    graph[a][b] = cost
+
+for k in range(1, n+1):
+    for a in range(1,n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b] )
+
+for a in range(1,n+1):
+    for b in range(1, n+1):
+        if graph[a][b] ==INF:
+            print("INFINITY", end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print("")
