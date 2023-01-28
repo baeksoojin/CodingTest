4f5323d99679e889db10eb9f59fb36040c8f2b0a
sujin-create <qsoojinq@naver.com>
2023-01-28 18:18:20 +0900

    노선이 하나가 아니라 여러개일수도 있을 때의 플로이드 워셜/usr/local/bin/python3

---
+++ 최단경로/플로이드워셜/11404.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a==b:
+            graph[a][b] = 0
+
+
+for _ in range(m):
+    # a에서 b로 가는 비용을 입력받음
+    a,b,cost = map(int, input().split())
+    graph[a][b] = min(cost, graph[a][b])
+
+for k in range(1, n+1):
+    for a in range(1,n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b] )
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if graph[a][b] == INF:
+            print("0",  end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print()
