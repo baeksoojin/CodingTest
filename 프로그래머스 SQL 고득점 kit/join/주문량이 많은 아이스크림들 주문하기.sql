87389f2850afbbdd4f8fa4790cd4d43cd0d5de9d
sujin-create <qsoojinq@naver.com>
2023-02-20 00:52:19 +0900

    join/order by와 limit을 이용한 ranking 간결한 풀이

--- 프로그래머스 SQL 고득점 kit/join/주문량이 많은 아이스크림들 주문하기.sql
+++ 프로그래머스 SQL 고득점 kit/join/주문량이 많은 아이스크림들 주문하기.sql
@@ -6,3 +6,16 @@ group by FLAVOR) as J
 join FIRST_HALF as F on F.FLAVOR = J.FLAVOR) as R
 join FIRST_HALF as F on R.FLAVOR = F.FLAVOR
 where ranking <4
+
+'''
+subquery 활용 ranking제한걸기
+'''
+
+-- 다른 코드도 살펴보니 flavor를 중복제거해서 구한다음(inner join /left join 혹은 right join) 해서 다른 id여도 같은 flavor가 여러개 있어도 고려되도록함
+
+SELECT A.FLAVOR
+FROM FIRST_HALF A 
+left JOIN JULY B ON B.FLAVOR = A.FLAVOR
+GROUP BY A.FLAVOR
+ORDER BY SUM(A.TOTAL_ORDER) + SUM(B.TOTAL_ORDER) DESC
+LIMIT 3
