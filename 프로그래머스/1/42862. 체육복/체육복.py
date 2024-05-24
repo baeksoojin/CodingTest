95f8431e27f2300b12a66da6c8d27a4ff7d9dfd1
sujin <74058047+baeksoojin@users.noreply.github.com>
2024-05-24 23:38:19 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,28 +1,28 @@
-'''
-solve 2
-'''
-
 def solution(n, lost, reserve):
     answer = 0
     
-    reserve_set = set(reserve)
-    lost_set = set(lost)
-    same_set = reserve_set & lost_set
     
+    # 잂어버린 사람 lost, reserve 여벌
+    # 잃어버린 사람이 앞사람에게 여벌이 있다면
     
-    reserve = list(reserve_set - same_set)
-    lost_set = lost_set - same_set
+    # 앞사람에게 빌려주든 뒷사람에게 빌려주든 결과가 바뀌지 않음.
     
+    cloth = set(reserve) - set(lost) # 여벌이 남아있는 옷을 가지고 있는 학생
+    lost = set(lost) - set(reserve) # 잃어버린사람중 여벌이 있다면 그 사람은빌리지 않아도됨
+
+    cloth_list = list(cloth)
+    lost_list = list(lost)
+    count =0
     
-    for i in range(len(reserve)):
+    for i in lost:
+        if i-1 in set(cloth_list):
+            # 여벌의 옷을 빌리기
+            cloth_list.remove(i-1)
+            count+=1
+            continue
+        if i+1 in set(cloth_list):
+            cloth_list.remove(i+1)
+            count+=1
         
-        before = reserve[i] -1 
-        after = reserve[i]+1
-        
-        if before in lost_set:
-            lost_set.remove(before)
-        elif after in lost_set:
-            lost_set.remove(after)
-            
-        
-    return n-len(lost_set)
+    return n - (len(lost) - count)
+    
