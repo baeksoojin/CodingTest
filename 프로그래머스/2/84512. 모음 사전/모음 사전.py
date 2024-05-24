df8dd51723662ffc350db32582e4fcb4713e9920
sujin <74058047+baeksoojin@users.noreply.github.com>
2024-05-24 23:20:07 +0900

    [level 2] Title: 모음 사전, Time: 2.19 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,19 +1,18 @@
-from itertools import product
-
+import itertools
 
 def solution(word):
     answer = 0
     
-    # 모든 가능한 조합을 저장
-    answers = set()
-    words = [ 'A', 'E', 'I', 'O', 'U']
+    # 백트래킹 느낌 -> 중복제거 순열조합으로 풀이
+    base = ['A', 'E', 'I', 'O', 'U']
     
-    for i in range(1,6):
-        product_list = list(product(words, repeat = i))
-        for p in product_list:
-            word_temp = ''.join(p)
-            answers.add(word_temp)
-            
-    answer_list = list(answers)
-    answer_list.sort()
-    return answer_list.index(word)+1
+    word_set = set()
+    for i in range(1, 6):
+        
+        words = list(itertools.product(base, repeat = i))
+        for w in words:
+            word_set.add(''.join(w))
+    
+    word_list = list(word_set)
+    word_list.sort()
+    return word_list.index(word)+1
