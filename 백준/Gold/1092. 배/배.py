a8339d4ad47c1201b9ee34275402d88f2a939ed1
sujin <74058047+baeksoojin@users.noreply.github.com>
2024-06-11 00:07:23 +0900

    [Gold V] Title: 배, Time: 3656 ms, Memory: 110564 KB -BaekjoonHub

---
+++ 백준/Gold/1092. 배/배.py
@@ -0,0 +1,29 @@
+n = int(input())
+c_list = list(map(int, input().split())) # 크레인
+m = int(input())
+b_list = list(map(int, input().split())) # 박스의 무게
+
+if max(b_list) > max(c_list):
+    # -1처리
+    print(-1)
+else:
+    # 오름차순 정렬
+    c_list.sort(reverse= True)
+    b_list.sort(reverse = True)
+    time = 0
+    index= 0 
+    while b_list:
+        
+        time+=1
+
+        # 크래인 무게보다 더 적은 것 중에서 가장 큰 것을 제거
+        for c_w in c_list:
+            for b_w in b_list:
+                if b_w <= c_w:
+                    b_list.remove(b_w)
+                    break
+
+
+
+    
+    print(time)
