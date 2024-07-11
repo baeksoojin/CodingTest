66f36da78a4060c6a557a606890f446cb43d42bb
sujin <74058047+baeksoojin@users.noreply.github.com>
2024-07-11 17:34:30 +0900

    [Gold IV] Title: 부분합, Time: 124 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,23 +1,31 @@
-n, s = map(int, input().split())
+n,s = map(int, input().split())
+
 n_list = list(map(int, input().split()))
 
-prefix_sum = [0] * (n + 1)
-for i in range(1, n + 1):
-    prefix_sum[i] = prefix_sum[i - 1] + n_list[i - 1]
+prefix_sum = [0]*(n)
+prefix_sum[0] = n_list[0]
+for i in range(1,n):
+    prefix_sum[i] = prefix_sum[i-1] + n_list[i]
+prefix_sum = [0] + prefix_sum
 
-# Two pointer
-left, right, min_len = 0, 1, float('inf')
+# two pointer
+
+left, right, current_sum = 0, 0, 0
+min_len = 10e9
 
 while right <= n:
+
     current_sum = prefix_sum[right] - prefix_sum[left]
-
+    
     if current_sum >= s:
-        min_len = min(min_len, right - left)
-        left += 1
+        min_len = min(right-left, min_len)
+        left+=1
     else:
-        right += 1
+        right+=1
+        
 
-if min_len == float('inf'):
+    
+if min_len == 10e9:
     print(0)
 else:
     print(min_len)
