06b8f4c5e5551ce2141d1de3dae8634c2fc3dc01
sujin <74058047+baeksoojin@users.noreply.github.com>
2024-09-20 17:24:33 +0900

    [level 2] Title: 전화번호 목록, Time: 961.26 ms, Memory: 262 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,25 @@
+def solution(phone_book):
+    answer = True
+    
+#     for i in range(len(phone_book)):
+#         check_num = phone_book[i]
+#         check_size = len(check_num)
+#         for j in range(len(phone_book)):
+
+#             if check_num == phone_book[j][:check_size] and i!=j:
+#                 print(check_num)
+#                 answer = False
+
+    dic = {}
+
+    for i in range(len(phone_book)):
+        
+        phone_num = phone_book[i]
+        for j in range(len(phone_num)-1):
+            dic[phone_num[:j+1]]=1
+            
+    for p in phone_book:
+        if p in dic.keys():
+            return False
+
+    return answer
